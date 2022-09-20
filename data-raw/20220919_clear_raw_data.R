1ffcd9c545e1224d04b7ed6c90639574b912b426
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-09-20 19:55:55 -0300

    add new phases of data cleaning

--- data-raw/20220919_clear_raw_data.R
+++ data-raw/20220919_clear_raw_data.R
@@ -4,14 +4,11 @@
 
 raw_data <- readr::read_table(
     file = "data-raw/conversa_whats.txt",
-    col_names = FALSE,
-    # skip = 4
+    col_names = FALSE
 )
 
 
-
 # Transform ---------------------------------------------------------------
-
 
 # ajustar colunas e observações iniciais
 tidied_data <- raw_data |>
@@ -40,3 +37,52 @@ tidied_data <- raw_data |>
 
 # ver estrutura
 tidied_data |> dplyr::glimpse()
+tidied_data |> viewxl::view_in_xl()
+
+# Clear mensage column ----------------------------------------------------
+
+tidied_data |>
+    # retirar linhas iniciais
+    dplyr::slice(-c(1:4)) |>
+    # remover usuário 'Maykon'
+    dplyr::filter(usuario != "Maykon") |>
+    dplyr::mutate(
+
+        # remover pontuação
+        usuario = stringr::str_replace_all(
+            string = usuario,
+            pattern = ":",
+            replacement = ""
+        ),
+
+        # extrair tudo que que não seja o nome da usuária
+        usuario_info_extra = dplyr::case_when(
+            usuario != "Digníssima" ~ usuario,
+            TRUE ~ NA_character_
+        ),
+
+        # limpar a coluna de usuario
+        usuario = dplyr::case_when(
+            usuario != "Digníssima" ~ "Digníssima",
+            TRUE ~ usuario
+        ),
+
+        # criar coluna para identificar lantus
+        lantus = dplyr::case_when(
+            stringr::str_detect(
+                string = conteudo_mensagem,
+                pattern = "[lL]antus") ~ TRUE,
+            TRUE ~ FALSE
+        ),
+
+        # criar coluna para identificar rápida
+        rapida = dplyr::case_when(
+            stringr::str_detect(
+                string = conteudo_mensagem,
+                pattern = "[Rr][áa]pida") ~ TRUE,
+            TRUE ~ FALSE
+        )
+    ) |>
+
+    viewxl::view_in_xl()
+
